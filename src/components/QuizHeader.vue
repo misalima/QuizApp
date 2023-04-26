7448c3ff655b3c0f0c466c49c371649e9be87209
Misael Lima <misael.alisson14@gmail.com>
2023-04-26 08:00:21 -0300

    implement questions rendering and progress bar behavior

---
+++ src/components/QuizHeader.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+    const {questionStatus, barProgress} = defineProps(['questionStatus', 'barProgress']);
+
+</script>
+
+<template>
+    <header>
+        <h2>Question {{questionStatus}}</h2>
+        <div class="bar">
+            <div class="completion" :style="{width: barProgress}"></div>
+        </div>
+    </header>
+</template>
+
+<style scoped>
+    header {
+        margin-bottom: 10px;
+        margin-top: 30px;
+    }
+
+    .bar {
+        width: 300px;
+        height: 50px;
+        border: 3px solid bisque;
+    }
+
+    .completion {
+        width: 0%;
+        height: 100%;
+        background-color: thistle;
+    }
+</style>
--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -1,78 +1,28 @@
+<script setup>
+    import QuizHeader from "../components/QuizHeader.vue";
+    import Question from "../components/Question.vue";
+    import { useRoute } from "vue-router";
+    import quizzes from "../data/quizes.json"
+    import { ref, computed } from "vue";
+
+    const route = useRoute();
+    const quizId = parseInt(route.params.id);
+    const currentQuestionIndex = ref(0);
+    const quiz = quizzes.find((q) => q.id === quizId);
+
+
+    const questionStatus = computed(() => `${currentQuestionIndex.value}/${quiz.questions.length}` );
+    const barProgress = computed(() => `${currentQuestionIndex.value / quiz.questions.length * 100}%`)
+
+</script>
+
 <template>
-    <div class="container">
-        <header>
-            <h2>Question 1/3</h2>
-            <div class="bar">
-                <div class="completion"></div>
-            </div>
-        </header>
+    <div>
+        <QuizHeader :questionStatus="questionStatus" :barProgress="barProgress"/>
         <div>
-            <div class="question-container">
-                <h1 class="question">
-                    What is the chemical value of table salt?
-                </h1>
-            </div>
-            <div class="options-container">
-                <div class="option">
-                    <p class="option-label">A</p>
-                    <div class="option-value">
-                        <p>NaCl</p>
-                    </div>
-                </div>
-            </div>
+            <Question :question="quiz.questions[currentQuestionIndex]" :options="quiz.options" />
         </div>
+        <button @click="currentQuestionIndex++">Next question</button>
     </div>
 </template>
 
-<style scoped>
-
-header {
-    margin-bottom: 10px;
-    margin-top: 30px;
-    
-}
-
-.bar {
-    width: 300px;
-    height: 50px;
-    border: 3px solid bisque;
-}
-
-.completion {
-    height: 100%;
-    width: 0%;
-    background-color: thistle;
-}
-
-.question-container {
-    margin-top: 20px;
-}
-
-.question {
-    font-size: 40px;
-    margin-bottom: 20px;
-}
-
-.option {
-    display: flex;
-    margin-bottom: 20px;
-    cursor: pointer;
-}
-
-.option-label {
-    background-color: bisque;
-    width: 50px;
-    height: 50px;
-    font-size: 30px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    color: black;
-}
-
-.option-value {
-    font-size: 30px;
-    width: 100%;
-    padding: 0 20px;
-}
-</style>
